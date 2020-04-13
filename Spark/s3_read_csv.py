3abce0a8b08e756694b6e34d4515f5e02f7012cb
Jacque Carvalho <ds.jacque@gmail.com>
2020-04-13 11:05:24 -0300

    Create s3_read_csv.py

---
+++ Spark/s3_read_csv.py
@@ -0,0 +1,41 @@
+#!/bin/bash Spark2.4, Python 3
+
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame, DynamicFrameReader, DynamicFrameWriter, DynamicFrameCollection
+from awsglue.job import Job
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+
+# create glue context from spark context
+glueContext = GlueContext(SparkContext.getOrCreate())
+spark = glueContext.spark_session
+
+# set custom logging on
+logger = glueContext.get_logger()
+...
+
+# write into the log file with:
+logger.info("Type log info here ...")
+
+# load table that is on S3 bucket
+load_table = spark
+            .read
+            .format("com.databricks.spark.csv")
+            .option("header", "true")
+            .option("inferSchema", "true")
+            .option("sep", "|")
+            .load("s3://bucket/path/folder/")
+
+logger.info("load table ...")
+
+# creat an alias for the table
+df = load_table.alias("df")
+
+# set table as distinct
+df = df.distinct().cache()
+
+
+
