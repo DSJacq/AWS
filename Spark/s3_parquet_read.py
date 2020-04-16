b77b3430ec18297c01e17c976458272df0538aef
Jacque Carvalho <ds.jacque@gmail.com>
2020-04-16 16:16:09 -0300

    Create s3_parquet_read.py

---
+++ Spark/s3_parquet_read.py
@@ -0,0 +1,19 @@
+#!/bin/bash Spark2.4, Python 3
+
+import sys
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame, DynamicFrameReader, DynamicFrameWriter, DynamicFrameCollection
+from awsglue.job import Job
+
+# create glue context from spark context
+glueContext = GlueContext(SparkContext.getOrCreate())
+spark = glueContext.spark_session
+
+# ead parquet data from S3 using spark + glue context
+df_raw = spark.read.parquet("s3://bucket/folder/folder/*")
+
+# read parquet data from S3 using glueContext
+df = df_raw.cache()
